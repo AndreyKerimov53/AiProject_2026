cd6ee62a597b41aa2feceefebbfdc64095a342af
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-02 22:33:03 +0300

    2.7 Керимов А и-3-24

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,62 @@
 <template>
   <v-app>
     <!-- Боковое меню -->
-    <v-navigation-drawer app></v-navigation-drawer>
+    <v-navigation-drawer app v-model="drawer">
+      <!-- Заголовок -->
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            КИПУ
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Учебный проект
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <!-- Разделитель -->
+      <v-divider></v-divider>
+
+      <!-- Динамическое меню в дровере -->
+      <v-list dense>
+        <v-list-item
+          v-for="link in links"
+          :key="link.title"
+        >
+          <template v-slot:prepend>
+            <v-icon :icon="link.icon"></v-icon>
+          </template>
+          <v-list-item-title>{{ link.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
     
     <!-- Верхняя панель -->
     <v-app-bar app dark color="primary">
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       
       <v-spacer></v-spacer>
       
-      <!-- Кнопки в панели -->
+      <!-- Динамическое меню в хедере -->
       <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn>
-          <v-icon start icon="mdi-gift-outline"></v-icon>
-          Link One
+        <v-btn
+          v-for="link in links"
+          :key="link.title"
+          variant="text"
+        >
+          <v-icon
+            start
+            :icon="link.icon"
+          ></v-icon>
+          {{ link.title }}
         </v-btn>
-        <v-btn>Link Two</v-btn>
-        <v-btn>Link Three</v-btn>
       </v-toolbar-items>
     </v-app-bar>
     
     <!-- Основная область -->
     <v-main>
       <v-container>
-        <!-- Здесь будет контент -->
+        <router-view></router-view>
       </v-container>
     </v-main>
   </v-app>
@@ -31,6 +64,18 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      drawer: false,
+      links: [
+        {title: "Login", icon: "mdi-gift-outline", url: "/login"},
+        {title: "Registration", icon: "mdi-gift-outline", url: "/registration"},
+        {title: "Orders", icon: "mdi-gift-outline", url: "/orders"},
+        {title: "New ad", icon: "mdi-gift-outline", url: "/new"},
+        {title: "My ads", icon: "mdi-gift-outline", url: "/list"}
+      ]
+    }
+  }
 }
 </script>
