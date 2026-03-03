1bd5ffe863acc3276899a34a2072d51d5435c340
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-03 19:20:33 +0300

    12.2 Керимов А и-3-24

--- src/store/user.js
+++ src/store/user.js
@@ -1,10 +1,26 @@
+class User {
+  constructor(id, email, password) {
+    this.id = id
+    this.email = email
+    this.password = password
+  }
+}
+
 export default {
   state: {
     user: null
   },
   mutations: {
+    setUser(state, payload) {
+      console.log(payload)
+      state.user = payload
+    }
   },
   actions: {
+    registerUser({ commit }, { email, password }) {
+      // Здесь будет запрос на сервер для регистрации
+      commit('setUser', new User(1, email, password))
+    }
   },
   getters: {
     user(state) {
