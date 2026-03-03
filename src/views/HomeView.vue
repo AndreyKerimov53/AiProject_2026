08171919b506c28e230390611d88a68c17548eef
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-03 11:48:19 +0300

    5.4 Керимов А и-3-24

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,13 +4,25 @@
     <v-row justify="center">
       <v-col cols="12" xs="12">
         <v-carousel>
-          <v-carousel-item cover></v-carousel-item>
+          <v-carousel-item
+            v-for="ad in ads"
+            :key="ad.id"
+            :src="ad.src"
+            cover
+          >
+            <!-- Кнопка-ссылка на детальный просмотр -->
+            <div class="ad-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">
+                {{ ad.title }}
+              </v-btn>
+            </div>
+          </v-carousel-item>
         </v-carousel>
       </v-col>
     </v-row>
   </v-container>
 
-  <!-- Вторая область - Grid с карточками -->
+  <!-- Вторая область - Grid с карточками (пока без изменений) -->
   <v-container>
     <v-row justify="center">
       <v-col cols="12" xs="12">
@@ -71,3 +83,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.ad-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background: rgb(0,0,0,0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-top-right-radius: 5px;
+  border-top-left-radius: 5px;
+}
+</style>
