bb222c4a8a7ff533e6dbaeafee0d29dccef26875
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-04 00:54:16 +0300

    17.2 Керимов А и-3-24

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -3,23 +3,18 @@
     <v-row>
       <v-col cols="12">
         <v-card class="mt-5">
-          <!-- Картинка из данных объявления -->
           <v-img
             height="400px"
             :src="ad.src"
             cover
           ></v-img>
-
-          <!-- Текст из данных объявления -->
           <v-card-text>
             <h1 class="text--primary mb-3">{{ ad.title }}</h1>
             <p>{{ ad.desc }}</p>
           </v-card-text>
-
-          <!-- Кнопки -->
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn class="warning" color="orange">Edit</v-btn>
+            <modal-dialog></modal-dialog>
             <v-btn class="success" color="green">Buy</v-btn>
           </v-card-actions>
         </v-card>
@@ -29,8 +24,13 @@
 </template>
 
 <script>
+import EditAdModal from './EditAdModal.vue'
+
 export default {
   props: ['id'],
+  components: {
+    'modal-dialog': EditAdModal
+  },
   computed: {
     ad() {
       return this.$store.getters.adById(this.id)
