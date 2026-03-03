973e8fbc0559e4019c23e889c9ffe7997f42b850
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-03 16:12:53 +0300

    9.1 Керимов А и-3-24

--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -1,7 +1,37 @@
 <template>
   <v-container>
-    <v-row justify="center">
-      <h1>Ad</h1>
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mt-5">
+          <!-- Блок 1: Картинка -->
+          <v-img
+            height="400px"
+            src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"
+            cover
+          ></v-img>
+
+          <!-- Блок 2: Текст -->
+          <v-card-text>
+            <h1 class="text--primary mb-3">Lorem Ipsum</h1>
+            <p>
+              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+              when an unknown printer took a galley of type and scrambled it to make a type 
+              specimen book. It has survived not only five centuries, but also the leap into 
+              electronic typesetting, remaining essentially unchanged. It was popularised in 
+              the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+              and more recently with desktop publishing software like Aldus PageMaker including 
+              versions of Lorem Ipsum.
+            </p>
+          </v-card-text>
+
+          <!-- Блок 3: Кнопки -->
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn class="warning">Edit</v-btn>
+            <v-btn class="success">Buy</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -10,6 +40,12 @@
 export default {
   data() {
     return {
+      // Временные данные
+      ad: {
+        src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        title: "Lorem Ipsum",
+        desc: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s..."
+      }
     }
   }
 }
