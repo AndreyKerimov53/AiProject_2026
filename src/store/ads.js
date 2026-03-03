44e9e92109b8b4c4255dd07b58c9de6841c20804
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-03 18:15:15 +0300

    11.2 Керимов А и-3-24

--- src/store/ads.js
+++ src/store/ads.js
@@ -32,8 +32,15 @@ export default {
     ]
   },
   mutations: {
+    createAd(state, payload) {
+      state.ads.push(payload)
+    }
   },
   actions: {
+    createAd({ commit }, payload) {
+      payload.id = Math.random()
+      commit('createAd', payload)
+    }
   },
   getters: {
     ads(state) {
@@ -47,7 +54,6 @@ export default {
     myAds(state) {
       return state.ads
     },
-    
     adById(state) {
       return id => {
         return state.ads.find(ad => ad.id == id)
