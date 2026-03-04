b1069380205dea96192cb6d982fe0ed862678a81
AndreyKerimov53 <andreykerimov@gmail.com>
2026-03-04 22:56:29 +0300

    18.4 Керимов А и-3-24

--- src/views/Shared/BuyAdModal.vue
+++ src/views/Shared/BuyAdModal.vue
@@ -44,8 +44,8 @@
         <v-col cols="12">
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn @click="onClose">Close</v-btn>
-            <v-btn @click="onSave" color="success">Buy It!</v-btn>
+            <v-btn @click="onClose" :disabled="localLoading">Close</v-btn>
+            <v-btn @click="onSave" color="success" :disabled="localLoading" :loading="localLoading">Buy It!</v-btn>
           </v-card-actions>
         </v-col>
       </v-row>
@@ -60,7 +60,8 @@ export default {
     return {
       modal: false,
       name: '',
-      phone: ''
+      phone: '',
+      localLoading: false
     }
   },
   methods: {
@@ -71,6 +72,7 @@ export default {
     },
     onSave() {
       if (this.name !== '' && this.phone !== '') {
+        this.localLoading = true
         this.$store.dispatch('createOrder', {
           name: this.name,
           phone: this.phone,
@@ -78,6 +80,7 @@ export default {
           userId: this.ad.userId
         })
         .finally(() => {
+          this.localLoading = false
           this.name = ""
           this.phone = ""
           this.modal = false
